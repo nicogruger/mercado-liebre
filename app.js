ca61332d444c5330b9592b133ccdb41412f65aa2
Nicolas Gruger <nicolas.gruger@hotmail.com>
2024-02-23 10:01:16 -0300

    Se agregó el formulario de registro al sitio web

--- app.js
+++ app.js
@@ -2,15 +2,22 @@ const express = require('express');
 const app = express();
 
 const path = require("path");
-const publicPath = path.resolve(__dirname, "./public");
-app.use(express.static(publicPath))
-  
+const publicPath = path.resolve(__dirname, "public");
+app.use(express.static(publicPath));
 
-  app.listen(3000, () => {
-    console.log('Servidor escuchando en el puerto 3000');
-  }); 
-  
-  app.get("/", (req,res) => {
-    res.sendFile(path.resolve("./views/index.html"))
-  });
+app.listen(3000, () => {
+  console.log('Servidor escuchando en el puerto 3000');
+});
+
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/index.html"));
+});
+
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/register.html"));
+});
+
+
   
